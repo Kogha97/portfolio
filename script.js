c565af4df12fc59dd57ae772886ba81ffc850b79
Kogha97 <poiuytrewq7991@gmail.com>
2023-11-15 00:00:32 +0000

    media queries added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const resumeButton = document.getElementById('resume')
-
+const resumeButtonHamburger = document.getElementById('resume2')
 resumeButton.addEventListener('click',openCV)
-
+resumeButtonHamburger.addEventListener('click',openCV)
 function openCV(){
     let cvPath = 'Joao_Augusto_-_Front_end_Developer.pdf'
 
